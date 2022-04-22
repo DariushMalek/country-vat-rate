76d14af0c03f553f7aa7f9fff1fa89020408ff1c
Dariush Malek <nomalek@gmail.com>
2022-04-22 17:05:56 +0430

    Add DbContext and configuration extenssion

---
+++ ApiSource/GlobalBlue.VATCalculator.Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using GlobalBlue.VATCalculator.Model;
+using GlobalBlue.VATCalculator.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GlobalBlue.VATCalculator.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<Country> Countries { get; set; }
+
+    public DbSet<CountryRate> CountryRates { get; set; }
+}
---
+++ ApiSource/GlobalBlue.VATCalculator.Data/Configuration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GlobalBlue.VATCalculator.Data;
+
+public static class Configuration
+{
+    public static IServiceCollection AddDataServices(this IServiceCollection services, string dbConnectionString)
+    {
+        void ConfigureDbContext(DbContextOptionsBuilder o) => o.UseSqlite(dbConnectionString);
+
+        return services
+            .AddDbContext<AppDbContext>(ConfigureDbContext);
+    }
+}
