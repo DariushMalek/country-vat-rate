fc6de57f9ab8ee2656d8a300ab9a8f153b36b81a
Dariush Malek <nomalek@gmail.com>
2022-04-22 19:44:26 +0430

    CountryRateInfo response model

---
+++ ApiSource/GlobalBlue.VATCalculator.Model/Response/CountryRateInfoResponse.cs
@@ -0,0 +1,13 @@
+﻿
+namespace GlobalBlue.VATCalculator.Model.Response;
+
+public class CountryRateInfoResponse
+{
+    public int Id { get; set; }
+
+    public string RateTitle { get; set; }
+
+    public decimal Rate { get; set; }
+
+    public int CountryId { get; set; }
+}
