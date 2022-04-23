4889114514ef387bbf1d6293456621d1287ca75f
Dariush Malek <nomalek@gmail.com>
2022-04-23 09:12:57 +0430

    VatCalculatorService methods and tests

--- ClientApp/src/app/services/vat-calculator/vat-calculator.service.spec.ts
+++ ClientApp/src/app/services/vat-calculator/vat-calculator.service.spec.ts
@@ -13,4 +13,22 @@ describe('VatCalculatorService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('calcByVat shoud return correct amounts', () => {
+    const amount = service.calcByVat(5850, 13);
+    const expected = {net: 45000, gross: 50850, vat: 5850}
+    expect(amount).toEqual(expected);
+  });
+
+  it('calcByGross shoud return correct amounts', () => {
+    const amount = service.calcByGross(50850, 13);
+    const expected = {net: 45000, gross: 50850, vat: 5850}
+    expect(amount).toEqual(expected);
+  });
+
+  it('calcByNet shoud return correct amounts', () => {
+    const amount = service.calcByNet(45000, 13);
+    const expected = {net: 45000, gross: 50850, vat: 5850}
+    expect(amount).toEqual(expected);
+  });
 });
--- ClientApp/src/app/services/vat-calculator/vat-calculator.service.ts
+++ ClientApp/src/app/services/vat-calculator/vat-calculator.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Amounts } from 'src/app/models/amounts.model';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,31 @@ import { Injectable } from '@angular/core';
 export class VatCalculatorService {
 
   constructor() { }
+
+  calcByVat(vat: number, vatRate: number): Amounts {
+    const net = (100 * vat) / vatRate;
+    return <Amounts>{
+      gross: net + vat,
+      net: net,
+      vat: vat
+    };
+  }
+
+  calcByGross(gross: number, vatRate: number): Amounts {
+    const net = Math.round(gross / (1 + vatRate / 100)) ;
+    return <Amounts>{
+      gross: gross,
+      net: net,
+      vat: gross - net
+    };
+  }
+
+  calcByNet(net: number, vatRate: number): Amounts {
+    const vat = (net * vatRate) / 100;
+    return <Amounts>{
+      gross: net + vat,
+      net: net,
+      vat: vat
+    };
+  }  
 }
